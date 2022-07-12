e9e37ebc412b31599ff0094bb7bd0a5cf6d9c651
Ivan Khoda <ivankhoda@gmail.com>
2022-07-12 21:53:51 +0300

    Fix gemfile

--- app/models/order.rb
+++ app/models/order.rb
@@ -2,5 +2,5 @@ class Order < ApplicationRecord
   belongs_to :client
   belongs_to :assignee
   has_many :services, dependent: :delete_all
-  serialize :positions, array: true, default: [].to_yaml
+  serialize :positions, Array, default: []
 end
